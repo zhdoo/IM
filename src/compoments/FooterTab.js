96399436078351ab9755c492f21d982b85795664
zuoqingvip <328103224@qq.com>
2018-01-12 14:35:32 +0800

    tab点击

--- src/compoments/FooterTab.js
+++ src/compoments/FooterTab.js
@@ -15,23 +15,35 @@ import {
 } from 'react-native';
 
 const { width } = Dimensions.get('window')
-var details = [
-    {id:1,title:'打招呼',icon:require('../images/data.png')},
-    {id:2,title:'关注',icon:require('../images/data.png')},
-    {id:3,title:'下一个',icon:require('../images/data.png')},
-    {id:4,title:'送礼物',icon:require('../images/data.png')},
-];
-
-renderItem=(item,i)=>{
-    return <TouchableHighlight key={i} onPress={} style={{flex:1,margin:5}}><View style={{flexDirection:'column',alignItems:"center",justifyContent: 'center'}}><Image style={{width:20,height:20}}  source={item.icon}/><Text>{item.title}</Text></View></TouchableHighlight>
+export default class FooterTab extends Component<{}> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: 5,
+            details :[
+                {id:1,title:'打招呼',icon:require('../images/data.png')},
+                {id:2,title:'关注',icon:require('../images/data.png')},
+                {id:3,title:'下一个',icon:require('../images/data.png')},
+                {id:4,title:'送礼物',icon:require('../images/data.png')},
+            ]
+        };
+    }
+    _onPressBtn(e){
+        this.props.resetUserid(1000)
+        console.log(e)
+    }
+    renderItem(item, i) {
+        return <TouchableHighlight key={i} onPress={this._onPressBtn.bind(this,i)} style={{flex: 1, margin: 5}}   underlayColor="rgba(0,0,0,0)">
+                    <View style={{flexDirection: 'column', alignItems: "center", justifyContent: 'center'}}>
+                        <Image style={{width: 20, height: 20}} source={item.icon}/><Text>{item.title}</Text></View>
+                </TouchableHighlight>
+    }
+    render(){
+        return (
+            <View style={styles.bottomTab}>{this.state.details.map((item, i) =>this.renderItem(item, i))}</View>
+        );
+    }
 }
-
-const FooterTab=()=>
-    <View style={styles.bottomTab}>
-        {
-            details.map((item,i)=>renderItem(item,i))
-        }
-    </View>
 const styles = StyleSheet.create({
     bottomTab:{
         width,
@@ -39,5 +51,3 @@ const styles = StyleSheet.create({
         flexDirection:'row',
     },
 });
-
-export default FooterTab;
