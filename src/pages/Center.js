8b8569843159a1b78c05368d2617401adc4419d1
zuoqingvip <328103224@qq.com>
2018-01-15 17:30:39 +0800

    1

--- src/pages/Center.js
+++ src/pages/Center.js
@@ -16,8 +16,8 @@ import {
 } from 'react-native';
 
 
-const { width } = Dimensions.get('window')
-
+const { width} = Dimensions.get('window')
+const itemWidth=Dimensions.get('window')/3-2
 export default class Center extends Component<{}> {
     static navigationOptions = {
         header: null,
@@ -27,13 +27,15 @@ export default class Center extends Component<{}> {
     render() {
         return (
             <ScrollView style={styles.centerTop}>
-                <Image source={require('../images/timg.jpeg')} style={{width,height:300}}/>
+                <Image source={require('../images/timg.jpeg')} style={{width,height:300,opacity:0.2,backgroundColor:'#fff'}}/>
                 <View style={styles.centerMyInfo}>
                     <Image source={require('../images/timg.jpeg')} style={styles.centerMyHeaderPic}/>
-                    <Text>我很帅的</Text>
-                    <Text>12321321</Text>
-                    <View>
-                        
+                    <Text style={styles.centerMyNickname}>我很帅的</Text>
+                    <Text style={styles.centerMyId}>12321321</Text>
+                    <View  style={styles.centerTopBtn} >
+                        <View style={styles.centerTopBtnItem}><Text>1</Text></View>
+                        <View style={styles.centerTopBtnItem,{backgroundColor:'#000'}}><Text>2</Text></View>
+                        <View style={styles.centerTopBtnItem}><Text>3</Text></View>
                     </View>
                 </View>
             </ScrollView>
@@ -60,7 +62,22 @@ const styles = StyleSheet.create({
     centerMyHeaderPic:{
         width:80,
         height:80,
-        backgroundColor:'red',
         borderRadius:40,
+        marginBottom:30,
+        borderWidth:2,
+        borderColor:'#fff'
+    },
+    centerMyNickname:{
+        width,
+        height:30,
+        lineHeight:30,
+        textAlign:'center',
+    },
+    centerTopBtn:{
+         flexDirection:'row',
+    },
+    centerTopBtnItem:{
+        width:itemWidth,
+        alignItems:'center',    
     }
 });
